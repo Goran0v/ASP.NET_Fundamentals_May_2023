a023cb67593d13e0b2031e32e35cc0180242c295
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-18 18:50:51 +0200

    Add the exercise

---
+++ Workshop 2/TextSplitter/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitter.ViewModels;
+
+namespace TextSplitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextSplitViewModel textViewModel)
+        {
+            return View(textViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextSplitViewModel textViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Index", textViewModel);
+            }
+
+            string[] words = textViewModel.TextToSplit!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string splitText = String.Join(Environment.NewLine, words);
+
+            textViewModel.SplitText = splitText;
+            return this.RedirectToAction("Index", textViewModel);
+        }
+    }
+}
